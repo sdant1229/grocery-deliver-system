811eff603b9bbf2305bc3d9001b9166d3e6113dd
sdant1229 <147424841+sdant1229@users.noreply.github.com>
2025-10-23 16:26:40 -0400

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -4,20 +4,18 @@ session_start();
 $product_id = $_POST['product_id'];
 $quantity = $_POST['quantity'];
 
-// Initialize cart if not set
 if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = [];
 }
 
-// Add or update quantity
 if (isset($_SESSION['cart'][$product_id])) {
     $_SESSION['cart'][$product_id] += $quantity;
 } else {
     $_SESSION['cart'][$product_id] = $quantity;
 }
 
-// Success message
 $_SESSION['message'] = "$quantity item(s) added to your cart!";
 header('Location: index.php');
 exit;
 ?>
+
