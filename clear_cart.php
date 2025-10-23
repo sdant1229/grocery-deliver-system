787622205a2027e49fb6e26eeb84b284ce14cfc0
sdant1229 <147424841+sdant1229@users.noreply.github.com>
2025-10-23 16:27:38 -0400

    Update clear_cart.php

--- clear_cart.php
+++ clear_cart.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
 
-// Clear the cart
 if (isset($_SESSION['cart'])) {
     unset($_SESSION['cart']);
 }
 
-// Redirect back to home page (optional)
 header("Location: index.php");
 exit();
 ?>
+
